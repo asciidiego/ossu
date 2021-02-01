094652dfd9d2478b73969332681e2169cb237565
Diego Rodriguez <diego.vincent.rodriguez@gmail.com>
2021-02-01 23:33:05 +0100

    docs: explain the file name

--- 01-how-to-code-simple-data/source-me.sh
+++ 01-how-to-code-simple-data/source-me.sh
@@ -1,6 +1,9 @@
 #!/bin/zsh
 
-N=$(ls | grep -E "^\d{2}" | grep ".rkt$" | wc -l)
+# Sourcing this file is necessary to retain the aliases of the 
+# interactive shell
+
+N=$(ls | grep -E "^\d{2}" | grep ".rkt$" | wc -l | awk '{print $1}')
 ga $(ls | grep $N | grep \.rkt$)
 gc -m "exercise: $(ls | grep $N | sed 's/.\{3\}//' | sed 's/\.rkt//')" && \
 gp
