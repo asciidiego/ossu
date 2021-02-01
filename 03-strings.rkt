8974e089f8dff93593455c48ce89c9daa90f3cb0
Diego Rodriguez <diego.vincent.rodriguez@gmail.com>
2021-02-01 22:53:39 +0100

    chore: remove redundant files from subject 1

--- 03-strings.rkt
+++
@@ -1,9 +0,0 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname 03-strings) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-; 03 - strings
-(string-append "Hello," " " "Ada Lovelace")
-
-;              "01234567"
-(string-length "Lovelace")
-(substring "Lovelace" 0 4)
