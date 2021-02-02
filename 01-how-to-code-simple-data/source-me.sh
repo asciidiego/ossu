dedfa1e15431607583bc3dfb866576349b494854
Diego Rodriguez <diego.vincent.rodriguez@gmail.com>
2021-02-02 23:07:59 +0100

    fix: auto-committer exercise counter

--- 01-how-to-code-simple-data/source-me.sh
+++ 01-how-to-code-simple-data/source-me.sh
@@ -5,5 +5,5 @@
 
 N=$(ls | grep -E "^\d{2}" | grep ".rkt$" | wc -l | awk '{print $1}')
 ga $(ls | grep $N | grep \.rkt$)
-gc -m "exercise: $(ls | grep $N | sed 's/.\{3\}//' | sed 's/\.rkt//')" && \
+gc -m "exercise: $(ls *.rkt | grep $N)" && \
 gp
